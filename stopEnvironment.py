5d40481fa1d52ac68d7a003991f2014b8fbd0b80
mseq <mseq@globo.com>
2021-08-24 11:53:06 -0300

    Debug level dinamico

--- stopEnvironment.py
+++ stopEnvironment.py
@@ -1,15 +1,30 @@
 from datetime import datetime, timedelta
-import json
 import boto3
 import logging
 
+# Take boto3 clients
+ssm = boto3.client('ssm')
+cfn = boto3.client('cloudformation')
+ec2 = boto3.client('ec2')
+
 # create logger
 logger = logging.getLogger('STOPENV')
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+
+# set debug level
+res = ssm.get_parameter(Name='DebugLevel-LambdaScripts')['Parameter']['Value']
+if res == "DEBUG":
+    logger.setLevel(logging.DEBUG)
+    ch.setLevel(logging.DEBUG)
+elif res == "INFO":
+    logger.setLevel(logging.INFO)
+    ch.setLevel(logging.INFO)
+elif res == "WARNING":
+    logger.setLevel(logging.WARNING)
+    ch.setLevel(logging.WARNING)
+else:
+    logger.setLevel(logging.NOTSET)
+    ch.setLevel(logging.NOTSET)
 
 # create formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -25,10 +40,6 @@ d = str(datetime.today().strftime('%Y%m%d'))
 h = str(datetime.today().strftime('%H:%M'))
 logger.debug(f"Starting script StopEnvironment {d} {h}")
 
-# Take boto3 clients
-ssm = boto3.client('ssm')
-cfn = boto3.client('cloudformation')
-ec2 = boto3.client('ec2')
 
 # Take the time planned to execute the script
 res = ssm.get_parameter(Name='ShutDown-SAPB1-Environment')['Parameter']['Value']
