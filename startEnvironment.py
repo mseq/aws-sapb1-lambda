3842dfc018b89f1bcbfcb3bd202b6984f8f2bf06
mseq <mseq@globo.com>
2021-08-24 18:21:27 -0300

    Added handler to use inside Lambda

---
+++ startEnvironment.py
@@ -0,0 +1,117 @@
+from datetime import datetime, timedelta
+import boto3
+import logging
+import time
+
+# def main():
+def lambda_handler(event, context):
+
+    # Take boto3 clients
+    ssm = boto3.client('ssm')
+    cfn = boto3.client('cloudformation')
+    ec2 = boto3.client('ec2')
+
+    # create logger
+    logger = logging.getLogger('STARTENV')
+    ch = logging.StreamHandler()
+
+    # set debug level
+    res = ssm.get_parameter(Name='DebugLevel-LambdaScripts')['Parameter']['Value']
+    if res == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+        ch.setLevel(logging.DEBUG)
+    elif res == "INFO":
+        logger.setLevel(logging.INFO)
+        ch.setLevel(logging.INFO)
+    elif res == "WARNING":
+        logger.setLevel(logging.WARNING)
+        ch.setLevel(logging.WARNING)
+    else:
+        logger.setLevel(logging.NOTSET)
+        ch.setLevel(logging.NOTSET)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+
+    d = str(datetime.today().strftime('%Y%m%d'))
+    h = str(datetime.today().strftime('%H:%M'))
+    logger.debug(f"Starting script StopEnvironment {d} {h}")
+
+
+    # Take the time planned to execute the script
+    res = ssm.get_parameter(Name='Start-SAPB1-Environment')['Parameter']['Value']
+
+    if res == h:
+
+        # Check if the image was already created, then if not create image/bkp and update Parameter Store
+        imgName=f"WinClient-IMG-{d}"
+        res = ec2.describe_images(
+            Filters=[
+                    {
+                        'Name': 'name', 
+                        'Values': [imgName]
+                    }
+                ]
+            )['Images']
+
+        if res:
+            logger.warning(f"Image already exists {res[0]['ImageId']}")
+            res = res[0]['ImageId']
+        else:
+            res = ssm.get_parameter(Name='CFN-NLB-WinClientInstance')['Parameter']['Value']
+            res = ec2.create_image(InstanceId=res, Name=imgName)['ImageId']
+            logger.info(f"Creating img {imgName}")
+
+        # Make sure the image is available
+        state = ec2.describe_images(
+            Filters=[
+                    {
+                        'Name': 'name', 
+                        'Values': [imgName]
+                    }
+                ]
+            )['Images'][0]['State']
+
+        logger.info(f"Image state is {state}")
+        while state != "available":
+            time.sleep(300)
+
+            state = ec2.describe_images(
+                Filters=[
+                        {
+                            'Name': 'name', 
+                            'Values': [imgName]
+                        }
+                    ]
+                )['Images'][0]['State']
+
+            logger.info(f"Image state is {state}")
+
+        # Execute Cloud Formation Stack
+        res = ssm.get_parameter(Name='CFN-NLB-StackName')['Parameter']['Value']
+        url = ssm.get_parameter(Name='CFN-NLB-TemplateUrl')['Parameter']['Value']
+        logger.info(f"Creating CloudFormation Stack {res}")
+        res = cfn.create_Stack(StackName=res, TemplateURL=url, Capabilities=['CAPABILITY_NAMED_IAM'], 
+                Tags=[
+                    {'Key': 'Name', 'Value': 'SAP HANA WinClient ELB'}, 
+                    {'Key': 'Product', 'Value': 'SAP B1'}, 
+                    {'Key': 'Department', 'Value': 'TI'}, 
+                    {'Key': 'Environment', 'Value': 'Production'}
+                ]
+            )
+
+        # Start NAT Instance 
+        res = ssm.get_parameter(Name='NatInstance-SAPB1-Environment')['Parameter']['Value']
+        logger.info(f"Starting NAT Instance {res}")
+        res = ec2.start_instances(InstanceIds=[res])
+
+
+# if __name__ == "__main__":
+#     main()
--- stopEnvironment.py
+++ stopEnvironment.py
@@ -2,103 +2,110 @@ from datetime import datetime, timedelta
 import boto3
 import logging
 
-# Take boto3 clients
-ssm = boto3.client('ssm')
-cfn = boto3.client('cloudformation')
-ec2 = boto3.client('ec2')
+# def main():
+def lambda_handler(event, context):
 
-# create logger
-logger = logging.getLogger('STOPENV')
-ch = logging.StreamHandler()
+    # Take boto3 clients
+    ssm = boto3.client('ssm')
+    cfn = boto3.client('cloudformation')
+    ec2 = boto3.client('ec2')
 
-# set debug level
-res = ssm.get_parameter(Name='DebugLevel-LambdaScripts')['Parameter']['Value']
-if res == "DEBUG":
-    logger.setLevel(logging.DEBUG)
-    ch.setLevel(logging.DEBUG)
-elif res == "INFO":
-    logger.setLevel(logging.INFO)
-    ch.setLevel(logging.INFO)
-elif res == "WARNING":
-    logger.setLevel(logging.WARNING)
-    ch.setLevel(logging.WARNING)
-else:
-    logger.setLevel(logging.NOTSET)
-    ch.setLevel(logging.NOTSET)
+    # create logger
+    logger = logging.getLogger('STOPENV')
+    ch = logging.StreamHandler()
 
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # set debug level
+    res = ssm.get_parameter(Name='DebugLevel-LambdaScripts')['Parameter']['Value']
+    if res == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+        ch.setLevel(logging.DEBUG)
+    elif res == "INFO":
+        logger.setLevel(logging.INFO)
+        ch.setLevel(logging.INFO)
+    elif res == "WARNING":
+        logger.setLevel(logging.WARNING)
+        ch.setLevel(logging.WARNING)
+    else:
+        logger.setLevel(logging.NOTSET)
+        ch.setLevel(logging.NOTSET)
 
-# add formatter to ch
-ch.setFormatter(formatter)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# add ch to logger
-logger.addHandler(ch)
+    # add formatter to ch
+    ch.setFormatter(formatter)
 
-
-d = str(datetime.today().strftime('%Y%m%d'))
-h = str(datetime.today().strftime('%H:%M'))
-logger.debug(f"Starting script StopEnvironment {d} {h}")
+    # add ch to logger
+    logger.addHandler(ch)
 
 
-# Take the time planned to execute the script
-res = ssm.get_parameter(Name='ShutDown-SAPB1-Environment')['Parameter']['Value']
+    d = str(datetime.today().strftime('%Y%m%d'))
+    h = str(datetime.today().strftime('%H:%M'))
+    logger.debug(f"Starting script StopEnvironment {d} {h}")
 
-if res == h:
-    # Kill Cloud Formation Stack
-    res = ssm.get_parameter(Name='CFN-NLB-StackName')['Parameter']['Value']
-    logger.info(f"Deleting Cloud Formation Stack: {res}")
-    res = cfn.delete_stack(StackName=res)
 
-    # Expunge Image Backup with Expiring Retention Period for 3 consecutive days
-    extdays = ssm.get_parameter(Name='RetentionPeriod-SAPB1-Environment')['Parameter']['Value']
-    for i in [0, 1, 2]:
-        days = int(extdays) + i
-        d = str((datetime.today() - timedelta(days=days)).strftime('%Y%m%d'))
+    # Take the time planned to execute the script
+    res = ssm.get_parameter(Name='ShutDown-SAPB1-Environment')['Parameter']['Value']
 
-        # Find and deregister the WinClientImg
-        imgName=f"WinClient-IMG-{d}"
-        res = ec2.describe_images(
-            Filters=[
-                    {
-                        'Name': 'name', 
-                        'Values': [imgName]
-                    }
-            ]
-        )['Images']
-        if res:
-            logger.info(f"Deleting IMG {d} {res[0]['ImageId']}")
-            ec2.deregister_image(res[0]['ImageId'])
-        else:
-            logger.warning(f"No WinClient image to delete {d}")
+    if res == h:
+        # Kill Cloud Formation Stack
+        res = ssm.get_parameter(Name='CFN-NLB-StackName')['Parameter']['Value']
+        logger.info(f"Deleting Cloud Formation Stack: {res}")
+        res = cfn.delete_stack(StackName=res)
 
-        # Find and deregister the HanaImg
-        imgName=f"SAPHanaMaster-IMG-{d}"
-        resh = ec2.describe_images(
-            Filters=[
-                    {
-                        'Name': 'name', 
-                        'Values': [imgName]
-                    }
-            ]
-        )['Images']
-        if res:
-            logger.info(f"Deleting IMG {d} {res[0]['ImageId']}")
-            ec2.deregister_image(res[0]['ImageId'])
-        else:
-            logger.warning(f"No Hana Master image to delete {d}")
+        # Expunge Image Backup with Expiring Retention Period for 3 consecutive days
+        extdays = ssm.get_parameter(Name='RetentionPeriod-SAPB1-Environment')['Parameter']['Value']
+        for i in [0, 1, 2]:
+            days = int(extdays) + i
+            d = str((datetime.today() - timedelta(days=days)).strftime('%Y%m%d'))
 
-    # Stop NAT Intance
-    res = ssm.get_parameter(Name='NatInstance-SAPB1-Environment')['Parameter']['Value']
-    logger.info(f"Stoping NAT Instance {res}")
-    res = ec2.stop_instances(InstanceIds=[res])
+            # Find and deregister the WinClientImg
+            imgName=f"WinClient-IMG-{d}"
+            res = ec2.describe_images(
+                Filters=[
+                        {
+                            'Name': 'name', 
+                            'Values': [imgName]
+                        }
+                ]
+            )['Images']
+            if res:
+                logger.info(f"Deleting IMG {d} {res[0]['ImageId']}")
+                ec2.deregister_image(res[0]['ImageId'])
+            else:
+                logger.warning(f"No WinClient image to delete {d}")
 
-    # Shutdown WinClient Instance
-    res = ssm.get_parameter(Name='CFN-NLB-WinClientInstance')['Parameter']['Value']
-    logger.info(f"Stoping WinClient Instance {res}")
-    res = ec2.stop_instances(InstanceIds=[res])
+            # Find and deregister the HanaImg
+            imgName=f"SAPHanaMaster-IMG-{d}"
+            resh = ec2.describe_images(
+                Filters=[
+                        {
+                            'Name': 'name', 
+                            'Values': [imgName]
+                        }
+                ]
+            )['Images']
+            if res:
+                logger.info(f"Deleting IMG {d} {res[0]['ImageId']}")
+                ec2.deregister_image(res[0]['ImageId'])
+            else:
+                logger.warning(f"No Hana Master image to delete {d}")
 
-    # Shutdown RDP Instance
-    res = ssm.get_parameter(Name='RDPInstance-SAPB1-Environment')['Parameter']['Value']
-    logger.info(f"Stoping RDP Instance {res}")
-    res = ec2.stop_instances(InstanceIds=[res])
+        # Stop NAT Intance
+        res = ssm.get_parameter(Name='NatInstance-SAPB1-Environment')['Parameter']['Value']
+        logger.info(f"Stoping NAT Instance {res}")
+        res = ec2.stop_instances(InstanceIds=[res])
+
+        # Shutdown WinClient Instance
+        res = ssm.get_parameter(Name='CFN-NLB-WinClientInstance')['Parameter']['Value']
+        logger.info(f"Stoping WinClient Instance {res}")
+        res = ec2.stop_instances(InstanceIds=[res])
+
+        # Shutdown RDP Instance
+        res = ssm.get_parameter(Name='RDPInstance-SAPB1-Environment')['Parameter']['Value']
+        logger.info(f"Stoping RDP Instance {res}")
+        res = ec2.stop_instances(InstanceIds=[res])
+
+
+# if __name__ == "__main__":
+#     main()
